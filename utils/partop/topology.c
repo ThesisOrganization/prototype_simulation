abeb84d9f494d60f8da891ed040747bc569af653
Marco Morella <marcomorella1@gmail.com>
2020-07-26 19:49:53 +0200

    Initial work on parser and topological route answering utils

---
+++ utils/partop/topology.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "header.h"
+
+//for now extremely simple function, just returning the node to send the data, will get messier.
+int getNext(int * array, int index)
+{
+  return(array[index]);
+}
