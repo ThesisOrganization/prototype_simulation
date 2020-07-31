7a93c82335d9ac6df0f68f46177d39791464e5bb
Nick1296 <mattianicolella@gmail.com>
2020-07-31 21:17:54 +0200

    revert(heap): restore heap implementation in priority_queue
    Signed-off-by: Nick1296 <mattianicolella@gmail.com>

---
+++ utils/priority_queue/heap/_heap.h
@@ -0,0 +1,25 @@
+#ifndef _HEAP_H
+#define	_HEAP_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+typedef struct _heap_entry{
+	double key;
+    void * payload;
+	int position;
+} _heap_entry;
+
+typedef struct {
+	_heap_entry ** array;
+	int size;
+	int used;
+	int is_min_heap;
+} _heap;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HEAP_H */
