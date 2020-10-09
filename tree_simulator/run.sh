93c839b23e01276e9388444fbb76441bfb48d946
silviodeigiudici <silviodeigiudici@gmail.com>
2020-10-09 17:41:42 +0200

    run.sh improved

--- tree_simulator/run.sh
+++ tree_simulator/run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#echo "simulation_time"
+simulation_time=5000
+number_lp=30
+
+
+if [ -n "$2" ]; then
+    number_lp=$2
+fi
+
+if [ -n "$3" ]; then
+    simulation_time=$3
+fi
+
 if [ "$1" == "valgrind" ]; then
-    valgrind --leak-check=full ./queues --sequential --lp 30 --simulation-time 5000
+    valgrind --leak-check=full ./queues --sequential --lp $number_lp --simulation-time $simulation_time
+elif [ "$1" == "gdb" ]; then
+    gdb --args ./queues --sequantial --lp $number_lp --simulation-time $simulation_time
 else
-    ./queues --sequential --lp 30 --simulation-time 5000
+    ./queues --sequential --lp $number_lp --simulation-time $simulation_time
 fi
