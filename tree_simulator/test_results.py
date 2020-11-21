abfb2628517110a784c249d41eb3610611466425
Silvio <deigiudici.1708962@studenti.uniroma1.it>
2020-11-21 02:08:54 +0100

    feat(Test-completed): we can now do tests on the tree simulator

--- tree_simulator/test_results.py
+++ tree_simulator/test_results.py
@@ -10,23 +10,60 @@ def print_warning(a,b):
     print(" =/= ", end="")
     print(b)
 
+PERCENTAGE_CHECK = 10.0
+
 def compare_values(a, b):
+
+    warning_check = False
+
     if(isinstance(a, str) and isinstance(b, str)):
         if(a != b):
-            print_warning(a,b)
-        return
-    if(a != b):
-        print_warning(a, b)
+            print_warning(a, b)
+            warning_check = True
+        return warning_check
+
+    if(isinstance(a, int) and isinstance(b, int)):
+        if(a != b):
+            print_warning(a, b)
+            warning_check = True
+        return warning_check
+
+    if(isinstance(a, float) and isinstance(b, float)):
+        if(a == 0.0 and b == 0.0):
+            return warning_check
+        percentage = (abs(a - b) / max(a, b) ) * 100.0
+        if(percentage > PERCENTAGE_CHECK):
+            print_warning(a, b)
+            print("Percentage: ", end="")
+            print(percentage)
+            warning_check = True
+        return warning_check
+
+    print("WARNING: values type not found")
+    return warning_check
 
 def compare_dicts(a, b):
+
+    warning_check_final = False
+
     if(not isinstance(a, Mapping) and not isinstance(b,Mapping)):
-        compare_values(a, b)
-        return
+        warning_check = compare_values(a, b)
+        warning_check_final = warning_check
+        return warning_check_final
+
     if(len(a) != len(b)):
         print("ERROR: len(a) != len(b)!")
-        return
+        warning_check = True
+        return warning_check_final
+
     for k in a:
-        compare_dicts(a[k], b[k])
+        warning_check = compare_dicts(a[k], b[k])
+        if warning_check:
+            print(k, end="")
+            print(", ", end="")
+        warning_check_final = warning_check_final or warning_check
+    
+    return warning_check_final
 
 
 simulator_file = "simulation_results.json"
@@ -54,7 +91,11 @@ compare = sorted(compare, key=lambda device: device["id"])
 
 #comparing each device in the list
 for i in range(0, number_elements_simulator):
-    compare_dicts(simulator[i], compare[i])
+    warning_check = compare_dicts(simulator[i], compare[i])
+    if warning_check:
+        print("id element: ", end="")
+        print(i)
+        print()
 
 
 
